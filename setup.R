6e689cf86cee59fb0c6fb9017f869ba345378a8d
Hauke Licht <hauke.licht@t-online.de>
2025-10-09 11:55:02 +0200

    aded restore to setup.R

--- setup.R
+++ setup.R
@@ -6,6 +6,7 @@ if (!"renv" %in% row.names(installed.packages())) {
 if (file.exists("renv.lock")) {
   renv::activate()
   renv::load()
+  renv::restore()
 } else {
   renv::init(bare = TRUE)
   pkgs <- readLines("requirements.txt")
